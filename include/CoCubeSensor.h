680ab2ed133555d8ea02392b164a02c25ec82f0f
LiangShuai <sliang23@m.fudan.edu.cn>
2024-07-23 00:58:19 +0800

    add motor encoders function

--- include/CoCubeSensor.h
+++ include/CoCubeSensor.h
@@ -2,8 +2,8 @@
  * @file CoCubeSensor.h
  * @author Liang Shuai (sliang23@m.fudan.edu.com)
  * @brief CoCube Position Sensor Library for MicroBlocks
- * @version 0.1
- * @date 2024-06-23
+ * @version 0.2
+ * @date 2024-07-15
  * @copyright Copyright (c) 2024
  * 
  */
@@ -55,4 +55,35 @@ class CoCubeSensor {
 
     unsigned char write_muti_;
     unsigned short trans_muti_data_[6];
+
+// Encoder
+   public:
+    void EncoderUpdate();
+    static void LeftEncoderCountCallback();
+    static void RightEncoderCountCallback();
+    float GetSpeedLeft();
+    float GetSpeedRight();
+
+   private:
+    unsigned short left_speed_;
+    unsigned short right_speed_;
+
+    float encoder_to_dis_;
+    unsigned long curr_time_;
+    unsigned long last_time_;
+    unsigned long last_left_encoder_;
+    unsigned long last_right_encoder_;
+
+    static unsigned long curr_left_encoder_;
+    static unsigned long curr_right_encoder_;
+
+// Speed Correcting
+   public:
+    void SpeedCorrecting(unsigned char start_x, unsigned char start_y, unsigned char end_x, unsigned char end_y);
+    unsigned short GetLeftCorrecting();
+    unsigned short GetRightCorrecting();
+   
+   private:
+    float velocity_left;
+    float velocity_right;
 };
