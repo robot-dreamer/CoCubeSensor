4f7afa14a4af2633c3b52c5ce67333031d495424
LiangShuai <sliang23@m.fudan.edu.cn>
2024-08-17 20:52:47 +0800

    add CoCube Status(up or down) and Index

--- include/CoCubeSensor.h
+++ include/CoCubeSensor.h
@@ -1,9 +1,9 @@
 /**
  * @file CoCubeSensor.h
  * @author Liang Shuai (sliang23@m.fudan.edu.com)
- * @brief CoCube Position Sensor Library for MicroBlocks
- * @version 0.2
- * @date 2024-07-15
+ * @brief CoCube Sensor Library for MicroBlocks
+ * @version 0.3
+ * @date 2024-08-17
  * @copyright Copyright (c) 2024
  * 
  */
@@ -11,50 +11,19 @@
 #pragma once
 
 #include <Arduino.h>
-#include <pthread.h>
 
 class CoCubeSensor {
    public:
     CoCubeSensor();
 
+// Sensor
     void Init();
     void Update();
-    void Release();
     int GetX();
     int GetY();
     int GetAngle();
-
-   private:
-    unsigned char PowerOn();
-    unsigned char DecoderInit();
-    unsigned char CheckReadData();
-    unsigned char ReceiveData(unsigned short *rec_data);
-    void ReceiveData();
-
-    unsigned char TransmitCmd(unsigned short cmd);
-    void TransmitData();
-    void ReceiveRespond();
-
-    unsigned char ReadDecInfo(unsigned short cmd, unsigned short *data1, unsigned short *data2);
-    unsigned char WriteDecInfo(unsigned short cmd, unsigned short *data1, unsigned short *data2);
-
-    unsigned char EnterSuspend();
-    unsigned char ExitSuspend();
-
-   private:
-    struct PositionInfo {
-        unsigned short x;
-        unsigned short y;
-        unsigned short angle;
-    };
-
-    PositionInfo position_info_;
-
-    unsigned short rec_buffer_[4];
-    unsigned short trans_cmd_;
-
-    unsigned char write_muti_;
-    unsigned short trans_muti_data_[6];
+    int GetIndex();
+    bool GetState();
 
 // Encoder
    public:
