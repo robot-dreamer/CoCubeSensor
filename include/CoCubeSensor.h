2465dfff73ad34890690d00b5a7cc6742225f0a9
LiangShuai <sliang23@m.fudan.edu.cn>
2024-08-22 17:30:24 +0800

    Improve CoCube position accuracy

--- include/CoCubeSensor.h
+++ include/CoCubeSensor.h
@@ -2,8 +2,8 @@
  * @file CoCubeSensor.h
  * @author Liang Shuai (sliang23@m.fudan.edu.com)
  * @brief CoCube Sensor Library for MicroBlocks
- * @version 0.3
- * @date 2024-08-17
+ * @version 0.4
+ * @date 2024-08-22
  * @copyright Copyright (c) 2024
  * 
  */
@@ -14,10 +14,10 @@
 
 class CoCubeSensor {
    public:
-    CoCubeSensor();
-
-// Sensor
+// Sensor Init and Update
     void Init();
+
+// Position Sensor
     void Update();
     int GetX();
     int GetY();
@@ -26,33 +26,7 @@ class CoCubeSensor {
     bool GetState();
 
 // Encoder
-   public:
     void EncoderUpdate();
-    static void LeftEncoderCountCallback();
-    static void RightEncoderCountCallback();
     float GetSpeedLeft();
     float GetSpeedRight();
-
-   private:
-    unsigned short left_speed_;
-    unsigned short right_speed_;
-
-    float encoder_to_dis_;
-    unsigned long curr_time_;
-    unsigned long last_time_;
-    unsigned long last_left_encoder_;
-    unsigned long last_right_encoder_;
-
-    static unsigned long curr_left_encoder_;
-    static unsigned long curr_right_encoder_;
-
-// Speed Correcting
-   public:
-    void SpeedCorrecting(unsigned char start_x, unsigned char start_y, unsigned char end_x, unsigned char end_y);
-    unsigned short GetLeftCorrecting();
-    unsigned short GetRightCorrecting();
-   
-   private:
-    float velocity_left;
-    float velocity_right;
 };
